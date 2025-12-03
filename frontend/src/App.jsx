8014293743467fa4c5f1226c981ff9f875c03793
erouro <erouro99@gmail.com>
2025-12-03 21:53:49 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import StoryPage from "./pages/StoryPage";
+import Donate from "./pages/Donate";
+import Subscribe from "./pages/Subscribe";
+import PremiumStories from "./pages/PremiumStories";
+import SubmitStory from "./pages/SubmitStory";
+import AdminLogin from "./admin/AdminLogin";
+import AdminStories from "./admin/AdminStories";
+import AdminCategories from "./admin/AdminCategories";
+import AdminSubscribers from "./admin/AdminSubscribers";
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="container" style={{paddingTop:12}}>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/story/:id" element={<StoryPage/>} />
+          <Route path="/donate" element={<Donate/>} />
+          <Route path="/subscribe" element={<Subscribe/>} />
+          <Route path="/premium-stories" element={<PremiumStories/>} />
+          <Route path="/submit-story" element={<SubmitStory/>} />
+
+          <Route path="/admin/login" element={<AdminLogin/>} />
+          <Route path="/admin/stories" element={<AdminStories/>} />
+          <Route path="/admin/categories" element={<AdminCategories/>} />
+          <Route path="/admin/subscribers" element={<AdminSubscribers/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
